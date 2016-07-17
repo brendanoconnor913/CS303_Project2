e052cb9db2ae77a44f869d9982f2d6691ee95b45
Brendan O'Connor <brendan.oconnor.913@gmail.com>
2016-07-17 16:19:30 -0500

    Minor changes to menu

--- Menu.cpp
+++ Menu.cpp
@@ -14,22 +14,26 @@ int main() {
 	char choice;
 
 	while(true){
+		cout << endl;
 		cout << "Make your Elevator: \n 1. Set Number of Floors \n"
-		<< "2. Set Simulation Time (clock cycles) \n 3. Set Passengers Arrival Rate \n"
+		<< " 2. Set Simulation Time (clock cycles) \n 3. Set Passengers Arrival Rate \n"
 		<< " 4. Run Simulation \n 5. Exit \n";
 		cout << "Make your selection: ";
 		cin >> choice;
 		
 		switch (choice) {
 			case '1': {	
+				cout << "Floors: ";
 				cin >> floors;
 				break; 
 				}
 			case '2': {
+				cout << "Time: ";
 				cin >> totalTime;
 				break;
 				}
 			case '3': {
+				cout << "Rate: ";
 				cin >> arrivalRate;
 				break;
 			}
